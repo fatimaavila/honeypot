033caf699956da3ece3b5cc473ceed8918e13984
fatimaavila <69205813+fatimaavila@users.noreply.github.com>
2023-11-22 19:19:26 -0600

    como funcion

--- honeypot.py
+++ honeypot.py
@@ -8,13 +8,14 @@ def main():
     server_sock.bind(('', 2222))
     server_sock.listen(223)
 
-    client_sock, client_addr = server_sock.accept()
-    print(f"Connection from {client_addr[0]}:{client_addr[1]}")
-    transport = paramiko.Transport(client_sock)
-    server_key= paramiko.RSAKey.from_private_key_file('key')
-    transport.add_server_key(server_key)
-    ssh = paramiko.ServerInterface()
-    transport.start_server(server=ssh)
+    while True:
+        client_sock, client_addr = server_sock.accept()
+        print(f"Connection from {client_addr[0]}:{client_addr[1]}")
+        transport = paramiko.Transport(client_sock)
+        server_key= paramiko.RSAKey.from_private_key_file('key')
+        transport.add_server_key(server_key)
+        ssh = paramiko.ServerInterface()
+        transport.start_server(server=ssh)
     ## prueba
     
 
