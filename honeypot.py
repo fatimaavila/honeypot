a7ec4aeee6eb41fc2cc72f4bf8f0a547eb6239b0
fatimaavila <69205813+fatimaavila@users.noreply.github.com>
2023-11-22 19:29:23 -0600

    ssh server

--- honeypot.py
+++ honeypot.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python
 import socket
 import paramiko
+import threading
+
+class SSHServer(paramiko.ServerInterface):
+
+    def check_auth_password(self, username: str, password: str) -> int:
+        print(f"{username}:{password}")
+        return paramiko.AUTH_FAILED
+
+def handle_connection(client_sock)
+    transport = paramiko.Transport(client_sock)
+    server_key= paramiko.RSAKey.from_private_key_file('key')
+    transport.add_server_key(server_key)
+    ssh = paramiko.ServerInterface()
+    transport.start_server(server=ssh)
 
 def main():
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,11 +25,7 @@ def main():
     while True:
         client_sock, client_addr = server_sock.accept()
         print(f"Connection from {client_addr[0]}:{client_addr[1]}")
-        transport = paramiko.Transport(client_sock)
-        server_key= paramiko.RSAKey.from_private_key_file('key')
-        transport.add_server_key(server_key)
-        ssh = paramiko.ServerInterface()
-        transport.start_server(server=ssh)
+        
     ## prueba
     
 
